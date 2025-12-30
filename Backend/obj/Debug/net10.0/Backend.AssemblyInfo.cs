a733802758f7853fe008e92339112c1498f55621
Mirza Hodžić <hodzic04mirza@gmail.com>
2025-12-30 10:31:00 +0100

    Configured poller for ipv4 connectons to the db

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423cb76df2042242185991422ef3b09c6f1db539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13689f6ae6e193a8025ca21791111d07f2428572")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
