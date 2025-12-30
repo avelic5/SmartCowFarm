9d9cc8f15529f4c3ee049845c37042a7f3db0a4b
Mirza Hodžić <hodzic04mirza@gmail.com>
2025-12-30 10:36:55 +0100

    resolving conflicts with C#

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13689f6ae6e193a8025ca21791111d07f2428572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a733802758f7853fe008e92339112c1498f55621")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
